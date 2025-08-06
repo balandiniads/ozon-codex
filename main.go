8a986feeb685b35a3e37b6d123a203ba31ebd364
balandiniads <adwpony@gmail.com>
2025-08-06 16:50:28 +0300

    Update main.go

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ func main() {
 	// Create a client with your Client-Id and Api-Key
 	// [Documentation]: https://docs.ozon.ru/api/seller/en/#tag/Auth
 	opts := []ozon.ClientOption{
-		ozon.WithAPIKey("63ed3fb3-dab9-4e97-82e4-604c446ce1ef"),
-		ozon.WithClientId("1344259"),
+		ozon.WithAPIKey("63ed3fb3-dab9-4e444f"),
+		ozon.WithClientId("44459"),
 	}
 	client := ozon.NewClient(opts...)
 
